dded1d79a0b8c7412b20235104f0fb6dd7ca47cf
MalazMutwallyMohammedArbab <malazarbab870@gmail.com>
2026-07-29 17:17:21 +0300

    modified config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,4 @@
-export const API_URL = process.env.REACT_APP_API_URL;
+export const API_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000/api"
+    : "https://al-malaz-lab-system-1.onrender.com/api";
